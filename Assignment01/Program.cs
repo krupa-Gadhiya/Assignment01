7c05fcad19c20c8480bf6d007d4c0ac676ee8b7a
krupa-Gadhiya <krupavaghasiya93@gmail.com>
2021-06-17 15:22:57 -0400

    Console Application created

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -6,7 +6,110 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r = new Rectangle();
+            int choice;
+            int length;
+            int width;
+            length = ValidInput("Length");
+            width = ValidInput("width");
+
+            Console.WriteLine($"Your lenght is {length} and width is {width}.\n");
+            Rectangle customRect = new Rectangle(length, width);
+            r = customRect;
+
+            choice = MenuSelection();
+
+            while (choice != 7)
+            {
+                int result;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine($"Length is: {r.GetLength()}\n");
+                        break;
+                    case 2:
+                        result = ValidInput("length");
+                        r.SetLength(result);
+                        break;
+                    case 3:
+                        Console.WriteLine($"width is: {r.GetWidth()}\n");
+                        break;
+                    case 4:
+                        result = ValidInput("width");
+                        r.SetWidth(result);
+                        break;
+                    case 5:
+                        Console.WriteLine($"Rectangle Perimeter of {r.GetLength()} and {r.GetWidth()} is: {r.GetPerimeter()}\n");
+                        break;
+                    case 6:
+                        Console.WriteLine($"Rectangle Area of {r.GetLength()} and {r.GetWidth()} is: {r.GetArea()}\n");
+                        break;
+                    default:
+                        break;
+                }
+
+                choice = MenuSelection();
+
+            }
+        }
+        public static int MenuSelection()
+        {
+            string takeInput = "";
+            bool validSelection = false;
+            int takeInpuInt;
+
+            while (validSelection == false)
+            {
+                Console.WriteLine("1 = Get Rectangle Length");
+                Console.WriteLine("2 = Change Rectangle Length");
+                Console.WriteLine("3 = Get Rectangle  width");
+                Console.WriteLine("4 = Change Rectangle width");
+                Console.WriteLine("5 = Get Rectangle Perimeter");
+                Console.WriteLine("6 = Get Rectangle Area");
+                Console.WriteLine("7 = Exit\n");
+
+                Console.WriteLine("Please select an option :\n");
+                takeInput = Console.ReadLine();
+                takeInpuInt = int.Parse(takeInput);
+
+                if (takeInpuInt <= 0 || takeInpuInt > 7)
+                {
+                    Console.WriteLine("Chosse correct option\n");
+                }
+                else
+                {
+                    validSelection = true;
+                }
+            }
+
+            Console.WriteLine();
+            return int.Parse(takeInput);
+        }
+
+        public static int ValidInput(string userNumber)
+        {
+            int valiDate = 0;
+            bool isValid = false;
+
+            while (isValid == false)
+            {
+                Console.WriteLine($"Please enter the  {userNumber} :");
+                string takeInput = Console.ReadLine();
+                valiDate = int.Parse(takeInput);
+
+                if (valiDate <= 0)
+                {
+                    Console.WriteLine("That's not a valid input please, please Enter value again.\n");
+                }
+                else
+                {
+                    isValid = true;
+                    Console.WriteLine($"Your {userNumber} is: {valiDate}\n");
+                }
+            }
+
+            return valiDate;
         }
     }
 }
